6f930d4a8e6d4185b9704c5fde0e591b1d848488
Jeff Yutzler <jyutzler@yahoo.com>
2016-08-29 16:31:22 -0400

    adding preflight function for #6765

--- http.go
+++ http.go
@@ -252,3 +252,16 @@ func HTTPOut(w http.ResponseWriter, output string, code int) {
 	w.WriteHeader(code)
 	w.Write([]byte(output))
 }
+
+// Preflight sets up the CORS stuff and
+// returns TRUE if this is an OPTIONS request
+func Preflight(w http.ResponseWriter, r *http.Request) bool {
+	if origin := r.Header.Get("Origin"); origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+
+	return (r.Method == "OPTIONS")
+}
